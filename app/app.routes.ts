1d521e91ac4670ed1c3b6af0c7d360e2e5097b19
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2017-03-08 15:58:28 -0700

    added 404 view component, template, updated route

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -2,13 +2,15 @@ import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./components/home-component";
 import {AboutComponent} from "./components/about-component";
 import {SignUpComponent} from "./components/signup-component";
+import {NotFoundComponent} from "./components/notfound-component";
 
-export const allAppComponents = [HomeComponent, AboutComponent, SignUpComponent];
+export const allAppComponents = [HomeComponent, AboutComponent, SignUpComponent, NotFoundComponent];
 
 export const routes: Routes = [
 	{path: "", component: HomeComponent},
 	{path: "about", component: AboutComponent},
-	{path: "sign-up", component: SignUpComponent}
+	{path: "sign-up", component: SignUpComponent},
+	{path: "**", component: NotFoundComponent}
 ];
 
 export const appRoutingProviders: any[] = [];
