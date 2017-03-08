9b044056c35b2094cc264df0291f0a702ac0ecba
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2017-03-08 16:10:41 -0700

    added navbar view component, template, updated route

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -3,8 +3,9 @@ import {HomeComponent} from "./components/home-component";
 import {AboutComponent} from "./components/about-component";
 import {SignUpComponent} from "./components/signup-component";
 import {NotFoundComponent} from "./components/notfound-component";
+import {MainNavComponent} from "./components/mainnav-component";
 
-export const allAppComponents = [HomeComponent, AboutComponent, SignUpComponent, NotFoundComponent];
+export const allAppComponents = [HomeComponent, AboutComponent, SignUpComponent, NotFoundComponent, MainNavComponent];
 
 export const routes: Routes = [
 	{path: "", component: HomeComponent},
