0df5883ced4229cd18e0244b8ba9e9bb1618d6ee
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2017-03-08 15:28:14 -0700

    added google font to webpack template

--- webpack/index.php
+++ webpack/index.php
@@ -9,6 +9,10 @@ if(session_status() !== PHP_SESSION_ACTIVE) {
 		<meta charset="UTF-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+		<!-- Google Fonts -->
+		<link href='https://fonts.googleapis.com/css?family=Roboto:400,400italic,300,700italic,700|Playfair+Display:400,400italic' rel='stylesheet' type='text/css'>
+
 		<base href="<?php echo dirname($_SERVER["PHP_SELF"]) . "/"; ?>" />
 
 		<title>Meow App 2</title>
